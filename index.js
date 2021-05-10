7880171a6868765e108b58c7e657808d9d8cf4de
Kwangil Shin <kwangilshin@Kwangilui-MacBookAir.local>
2021-05-10 21:51:51 +0900

    로그인 post 작성 및 postman으로 api test / nodemon 설치

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser') //최신 express에는 bodyParser기능이 내장되어있다.
+const {User}  = require("./models/users")
+
+app.use(express.urlencoded({extended: true})); //express에서 제공하는 bodyParser기능
+
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://nike1010:nike1010@boilerplate.nrdwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -10,6 +16,18 @@ mongoose.connect('mongodb+srv://nike1010:nike1010@boilerplate.nrdwu.mongodb.net/
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면
+  //그것들을 DB에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
