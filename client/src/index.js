2ac2790c5b2a82ccda5eb56a2e7fa0f859df55ba
Kwangil Shin <kwangilshin@Kwangilui-MacBookAir.local>
2021-05-20 16:42:36 +0900

    css 프레임워크 ant design추가

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';  // css프레임워크 antdesign
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
