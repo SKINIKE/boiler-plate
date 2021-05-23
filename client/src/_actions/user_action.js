e538f506073018ac52789f86d555484449653054
Kwangil Shin <kwangilshin@Kwangilui-MacBookAir.local>
2021-05-23 22:15:33 +0900

    회원가입 기능 구현

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,8 @@
 import Axios from 'axios'
-import {LOGIN_USER} from './types'
+import {
+    LOGIN_USER,
+    REGISTER_USER
+} from './types'
 
 export function loginUser(dataToSubmit){
    const request= Axios.post('/api/users/login', dataToSubmit)
@@ -9,3 +12,12 @@ export function loginUser(dataToSubmit){
         payload: request
     }
 }
+
+export function registerUser(dataToSubmit){
+    const request= Axios.post('/api/users/register', dataToSubmit)
+         .then(response => response.data)
+     return{
+         type: REGISTER_USER,
+         payload: request
+     }
+ }
